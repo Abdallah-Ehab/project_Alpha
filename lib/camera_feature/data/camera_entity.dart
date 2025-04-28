cb785a3aa8d520c10e589ffd4a1acae9602cc918
eddypencil <eydopencil@gmail.com>
2025-04-28 22:29:34 +0300

    added from and to json to most

--- lib/camera_feature/data/camera_entity.dart
+++ lib/camera_feature/data/camera_entity.dart
@@ -1,6 +1,8 @@
 import 'dart:ui';
 
+import 'package:json_annotation/json_annotation.dart';
 import 'package:scratch_clone/entity/data/entity.dart';
+
 
 class CameraEntity extends Entity{
   double zoom;
@@ -16,6 +18,34 @@ class CameraEntity extends Entity{
     this.isEditorCamera = true,
   });
 
+  factory CameraEntity.fromJson(Map<String, dynamic> json) {
+    return CameraEntity(
+      name: json['name'] as String,
+      position: Entity.offsetFromJson(json['position'] as Map<String, dynamic>),
+      rotation: (json['rotation'] as num).toDouble(),
+      width: (json['width'] as num?)?.toDouble() ?? 500,
+      height: (json['height'] as num?)?.toDouble() ?? 500,
+      layerNumber: json['layerNumber'] as int? ?? 0,
+      zoom: (json['zoom'] as num?)?.toDouble() ?? 1.0,
+      isEditorCamera: json['isEditorCamera'] as bool? ?? true,
+    );
+  }
+
+  @override
+  Map<String, dynamic> toJson() {
+    return {
+      'type': 'camera',
+      'name': name,
+      'position': Entity.offsetToJson(position),
+      'rotation': rotation,
+      'width': width,
+      'height': height,
+      'layerNumber': layerNumber,
+      'zoom': zoom,
+      'isEditorCamera': isEditorCamera,
+    };
+  }
+
   void pan(Offset delta) {
     position += delta;
     notifyListeners();
@@ -25,4 +55,5 @@ class CameraEntity extends Entity{
     zoom = newZoom.clamp(0.2, 4.0);
     notifyListeners();
   }
+
 }
